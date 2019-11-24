b02136c4a8c8665301fc69b9773f4f4cf5f73289
Ramón Ventura <50932514+Ramon-Ventura@users.noreply.github.com>
2019-11-24 12:36:12 -0600

    Adding Register Activity

--- app/src/main/java/com/example/socialmediaapp/MainActivity.kt
+++ app/src/main/java/com/example/socialmediaapp/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.example.socialmediaapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    //Defining buttons
+    private lateinit var btnRegister : Button
+    private lateinit var btnLogin : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //Init
+        btnRegister = findViewById(R.id.buttonRegister)
+        btnLogin = findViewById(R.id.buttonLogin)
+
+        //Start Register Activity
+        btnRegister.setOnClickListener {
+            val intent = Intent(this,RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
