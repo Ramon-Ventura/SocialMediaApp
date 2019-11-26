bd2b0529a1808c0b25ac6157ac1930c9906040a7
Ramón Ventura <50932514+Ramon-Ventura@users.noreply.github.com>
2019-11-26 11:43:53 -0600

    Adding firebase implementation

--- app/src/main/java/com/example/socialmediaapp/RegisterActivity.kt
+++ app/src/main/java/com/example/socialmediaapp/RegisterActivity.kt
@@ -2,11 +2,45 @@ package com.example.socialmediaapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.Button
+import android.widget.EditText
 
 class RegisterActivity : AppCompatActivity() {
+    //Defining buttons
+    private lateinit var etEmail : EditText
+    private lateinit var etPassword : EditText
+    private lateinit var btnRegister : Button
+
+    //Firebase variable
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        //Init
+        etEmail = findViewById(R.id.editTextEmail)
+        etPassword = findViewById(R.id.editTextPassword)
+        btnRegister = findViewById(R.id.ar_buttonRegister)
+        //Register Listener
+        btnRegister.setOnClickListener {
+            val email = etEmail.text.toString().trim()
+            val password = etPassword.text.toString().trim()
+
+            //validate
+            if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                etEmail.error = "Invalid Email"
+                etEmail.isFocusable = true
+            }else if (etPassword.length() < 6) {
+                etPassword.error = "Short Password"
+                etPassword.isFocusable = true
+            }else{
+                registerUser(email,password)
+            }
+        }
+    }
+
+    //Register user to firebase collection
+    private fun registerUser(email: String, password: String) {
+
     }
 }
