41fcaf9e0bd8733fc4c64b9dfeebce4f6664e7ff
Ramón Ventura <50932514+Ramon-Ventura@users.noreply.github.com>
2019-12-10 21:42:26 -0600

    adding profile activity

--- app/src/main/java/com/example/socialmediaapp/RegisterActivity.kt
+++ app/src/main/java/com/example/socialmediaapp/RegisterActivity.kt
@@ -1,6 +1,9 @@
+@file:Suppress("DEPRECATION")
+
 package com.example.socialmediaapp
 
 import android.app.ProgressDialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Patterns
@@ -8,6 +11,7 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+
 
 class RegisterActivity : AppCompatActivity() {
     //Defining buttons
@@ -18,8 +22,8 @@ class RegisterActivity : AppCompatActivity() {
     //Firebase variable
     private lateinit var auth: FirebaseAuth
 
-    //Progress Diaglog
-    lateinit var  progressDialog : ProgressDialog
+    //Progress Dialog
+    private lateinit var  progressDialog : ProgressDialog
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -62,11 +66,13 @@ class RegisterActivity : AppCompatActivity() {
         auth.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
-                    // Sign in success, update UI with the signed-in user's information
+                    // Sign in success, dismiss dialog and start profile activity
                     progressDialog.dismiss()
                     val user = auth.currentUser
                     if (user != null) {
                         Toast.makeText(this,"Registered \n"+user.email, Toast.LENGTH_SHORT).show()
+                        startActivity(Intent(this,ProfileActivity::class.java))
+                        finish()
                     }
                 } else {
                     // If sign in fails, display a message to the user.
@@ -77,5 +83,10 @@ class RegisterActivity : AppCompatActivity() {
 
                 // ...
             }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return super.onSupportNavigateUp()
     }
 }
